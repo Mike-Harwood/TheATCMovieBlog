d14553fc76c6898c089f261de22ccbda2b2797b3
Mike-Harwood <92137315+Mike-Harwood@users.noreply.github.com>
2022-10-27 21:25:06 +1300

    Created 2 classes Movie and Cast

---
+++ Models/Cast.cs
@@ -0,0 +1,20 @@
+﻿namespace TheATCMovieBlog.Models
+{
+    public class Cast
+    {
+
+        public Guid ID { get; set; }
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? ScreenName { get; set; }
+
+        public Guid MovieID { get; set; }
+
+        public Movie? Movie { get; set; }
+
+
+
+    }
+}
